eb33c553be7e83b55341c670ddc17b798a0d2ace
balash <ballasha@yandex.ru>
2023-04-29 12:15:00 +0500

    new car controller, camera cinemachine, city location

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider[] wheel_col;
+    public Transform[] wheels;
+    float torque = 100;
+    float angle = 45;
+
+    void Update()
+    {
+        for (int i = 0; i < wheel_col.Length; i++)
+        {
+            wheel_col[i].motorTorque = Input.GetAxis("Vertical") * torque;
+            if (i == 0 || i == 1)
+            {
+                wheel_col[i].steerAngle = Input.GetAxis("Horizontal") * angle;
+            }
+
+            var pos = transform.position;
+            var rot = transform.rotation;
+            wheel_col[i].GetWorldPose(out pos, out rot);
+            wheels[i].position = pos;
+            wheels[i].rotation = rot;
+
+        }
+
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                foreach (var i in wheel_col)
+                {
+                    i.brakeTorque = 2000;
+                }
+            }
+            else
+            {
+                //reset the brake torque when another key is pressed
+                foreach (var i in wheel_col)
+                {
+                    i.brakeTorque = 0;
+                }
+
+            }
+        }
+    }
+}
