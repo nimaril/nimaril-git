ca51b07a3e87af347c93b32a7aec3e16de9481e2
balash <ballasha@yandex.ru>
2023-05-11 16:28:18 +0500

    test commit idk what happened here

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,7 @@ public class Main : MonoBehaviour
     
     public void LoadSettings()
     {
+        Debug.Log("Settings");
         SceneManager.LoadScene(3);
     }
 
